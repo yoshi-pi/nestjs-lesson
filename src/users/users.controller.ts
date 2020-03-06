b67f1042b24316e97938052424ba342cb7f5c834
Yoshipe <sho318go@gmail.com>
2020-03-06 19:53:10 +0900

    26:23 DTOを使って、Bodyの型を定義する方法

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,10 +1,11 @@
 import { Controller, Post, Body, Param, Query, Response } from '@nestjs/common';
+import { CreateUserDto } from './dto/create-user.dto';
 
 @Controller('users')
 export class UsersController {
 
   @Post()
-  create(@Body() createUsers) {
-    return createUsers
+  create(@Body() createUser: CreateUserDto) {
+    return createUser
   }
 }
