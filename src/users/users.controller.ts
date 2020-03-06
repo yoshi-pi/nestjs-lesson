b9f3ecd1be41df5508f9cc2d64f7d4c11ffd5ee3
Yoshipe <sho318go@gmail.com>
2020-03-06 20:27:25 +0900

    26:23 DTOを使って、Bodyの型を定義する方法

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,11 +1,17 @@
-import { Controller, Post, Body, Param, Query, Response } from '@nestjs/common';
+import { Controller, Post, Body, Param, Query, Response, Get } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
+import { UsersService } from './users.service';
 
 @Controller('users')
 export class UsersController {
+  constructor(private readonly usersService: UsersService) { }
+  @Get()
+  findAll() {
+    return this.usersService.findAll();
+  }
 
   @Post()
   create(@Body() createUser: CreateUserDto) {
-    return createUser
+    return this.usersService.create(createUser);
   }
 }
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,13 @@
 import { Injectable } from '@nestjs/common';
+import { CreateUserDto } from './dto/create-user.dto';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+  users: CreateUserDto[] = [];
+  create(user: CreateUserDto) {
+    this.users.push(user);
+  }
+  findAll() {
+    return this.users;
+  }
+}
