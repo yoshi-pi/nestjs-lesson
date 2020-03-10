2b74869f25196508ff39d5e57c4ee0ea461a4b9c
Yoshipe <sho318go@gmail.com>
2020-03-10 15:01:07 +0900

    authモジュールはこうして作る

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { MongooseModule } from "@nestjs/mongoose";
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [UsersModule, MongooseModule.forRoot('mongodb+srv://yoshipi:1234567890@cluster0-s9egy.gcp.mongodb.net/nest?retryWrites=true&w=majority')],
+  imports: [UsersModule, MongooseModule.forRoot('mongodb+srv://yoshipi:1234567890@cluster0-s9egy.gcp.mongodb.net/nest?retryWrites=true&w=majority'), AuthModule],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ src/auth/auth.controller.ts
@@ -0,0 +1,13 @@
+import { Controller, Post, Body, ValidationPipe } from '@nestjs/common';
+import { AuthService } from './auth.service';
+import { CreateUserDto } from 'src/users/dto/create-user.dto';
+
+@Controller('auth')
+export class AuthController {
+  constructor(private readonly authService: AuthService) { }
+
+  @Post()
+  create(@Body(ValidationPipe) createUser: CreateUserDto) {
+    return this.authService.login(createUser);
+  }
+}
