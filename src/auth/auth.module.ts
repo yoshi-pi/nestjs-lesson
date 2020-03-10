f26f27639c22a76fabb52df5805ecdd046b460b3
Yoshipe <sho318go@gmail.com>
2020-03-10 16:12:25 +0900

    ログイン時のValidationはこうする

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -2,9 +2,11 @@ import { Module } from '@nestjs/common';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 import { JwtModule } from "@nestjs/jwt";
+import { UsersModule } from 'src/users/users.module';
 
 @Module({
   imports: [
+    UsersModule,
     JwtModule.register({
       secret: 'secret',
       signOptions: { expiresIn: '1h' }
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,14 +1,23 @@
 import { Injectable } from '@nestjs/common';
 import { CreateUserDto } from 'src/users/dto/create-user.dto';
 import { JwtService } from '@nestjs/jwt';
+import { UsersService } from 'src/users/users.service';
 
 @Injectable()
 export class AuthService {
-  constructor(private readonly jwtService: JwtService) { }
+  constructor(
+    private readonly jwtService: JwtService,
+    private readonly usersService: UsersService
+  ) { }
+  async validateUser({ username, password }: CreateUserDto) {
+    const user = this.usersService.findOne(username);
+  }
   async login(user: CreateUserDto) {
-    const payload = { username: user.username }
-    return {
-      'access_token': this.jwtService.sign(payload)
+    if (await this.validateUser(user)) {
+      const payload = { username: user.username }
+      return {
+        'access_token': this.jwtService.sign(payload)
+      }
     }
   }
 }
